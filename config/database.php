62cdc82af181669acd8a0a3ee23d7e6dd4b6df26
lzb <lzb@lzb-PC>
2016-12-23 15:53:11 +0800

    111 222 Signed-off-by:lzb <lzb@lzb-PC>

---
+++ config/database.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+
+    'fetch' => PDO::FETCH_CLASS,
+    'default' => env('DB_CONNECTION', 'mysql'),
+    'connections' => [
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => database_path('database.sqlite'),
+            'prefix' => '',
+        ],
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'test'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', '111111'),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => env('DB_PREFIX', 'blog_'),
+            'strict' => false,
+            'engine' => null,
+        ],
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+        ],
+    ],
+    /*
+      |--------------------------------------------------------------------------
+      | Migration Repository Table
+      |--------------------------------------------------------------------------
+      |
+      | This table keeps track of all the migrations that have already run for
+      | your application. Using this information, we can determine which of
+      | the migrations on disk haven't actually been run in the database.
+      |
+     */
+    'migrations' => 'migrations',
+    /*
+      |--------------------------------------------------------------------------
+      | Redis Databases
+      |--------------------------------------------------------------------------
+      |
+      | Redis is an open source, fast, and advanced key-value store that also
+      | provides a richer set of commands than a typical key-value systems
+      | such as APC or Memcached. Laravel makes it easy to dig right in.
+      |
+     */
+    'redis' => [
+
+        'cluster' => false,
+        'default' => [
+            'host' => env('REDIS_HOST', 'localhost'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 0,
+        ],
+    ],
+];
