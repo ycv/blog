21b8e5a923cfbe9667743681c127bc50843fb043
lzb <lzb@lzb-PC>
2016-12-26 14:56:00 +0800

    2升水 Signed-off-by:lzb <lzb@lzb-PC>

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">2222222222222222222 Be right back.</div>
             </div>
         </div>
     </body>
